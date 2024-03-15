0573f3e2560fdce02590e1834cff6a3a6785d8f7
Gustavo Enrique Varela Sojo <139945063+Ztrake1009@users.noreply.github.com>
2024-03-15 16:03:52 -0600

    Add files via upload

--- Prueba_Servo.ino
+++ Prueba_Servo.ino
@@ -2,8 +2,16 @@
 
 Servo myservo;
 
+int potpin = A0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+int servo = 9;
+int boton = 8;
+int estado;
+
 void setup(){
-  myservo.attach(9);
+  Serial.begin(9600);
+  myservo.attach(servo);
+  pinMode(boton, INPUT);
 }
 
 //De 0 a 89 es dirección antihoraria, siendo 0 la mayor velocidad.
@@ -11,12 +19,39 @@ void setup(){
 //De 91 a 180 es dirección horaria, siendo 180 la mayor velocidad.
 
 void loop(){
-  myservo.write(90); //Valor del servo para estar quieto.
-  delay(3000);
-  myservo.write(180); //
-  delay(1000);
-  myservo.write(90); //Valor del servo para estar quieto.
-  delay(3000);
-  myservo.write(0); //
-  delay(1000);
-}
+  val = analogRead(potpin);  // reads the value of the potentiometer (value between 0 and 1023)
+  val = map(val, 0, 1023, 0, 180);     // scale it for use with the servo (value between 0 and 180)
+  if (val <= 30){
+    //Serial.println("Antihorario");
+    myservo.write(80);
+
+  }
+  else if (val >= 150){
+    Serial.println("Horario");
+    myservo.write(100);
+  }
+  else {
+    Serial.println("Parada");
+    myservo.write(90);
+  }
+
+  estado = digitalRead(boton);
+  if (estado == HIGH){
+    Serial.println("Encendido");
+    myservo.write(100); //
+    delay(1693);
+  }
+  else {
+  //estado = LOW;
+  //Serial.println("Apagado");
+  }
+
+  //myservo.write(90); //Valor del servo para estar quieto.
+  //delay(3000);
+  //myservo.write(100); //
+  //delay(1625);
+  //myservo.write(90); //Valor del servo para estar quieto.
+  //delay(1500);
+  //myservo.write(0); //
+  //delay(1500);
+}   
